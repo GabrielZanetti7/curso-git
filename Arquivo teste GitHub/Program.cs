c3e1637c4ff8bb8a73ecc6cd751d1ee7e9823ec7
Gabriel Zanetti <gabriel.castilhos.zanetti@gmail.com>
2020-02-09 15:11:17 -0300

    Adicionado bom dia

--- Arquivo teste GitHub/Program.cs
+++ Arquivo teste GitHub/Program.cs
@@ -7,6 +7,8 @@ namespace Arquivo_teste_GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bom diaaaa");
         }
     }
 }
