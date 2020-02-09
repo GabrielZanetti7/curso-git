523c096145787286de7fcb98e71d36b3db8b40b5
Gabriel Zanetti <gabriel.castilhos.zanetti@gmail.com>
2020-02-09 14:36:23 -0300

    Projeto iniciado mostrando a mensagem hello world!

---
+++ Arquivo teste GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arquivo_teste_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
